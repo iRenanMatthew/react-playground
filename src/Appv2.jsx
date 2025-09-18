84cff43ba19c5bd111194e557713c66b0a6fc6d1
Renan Matthew Fajardo <renanmatthew206@gmail.com>
2025-09-19 00:33:26 +0800

    feat: react event handling

--- src/Appv2.jsx
+++ src/Appv2.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 
 const AddCounter = () => {
   const [counter, setCounter] = useState(0);
-  setTimeout (() => {
-    setCounter(counter + 1)
-  }, 1000)
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
 
-  console.log('rendering', counter)
+  console.log("rendering", counter);
 
-  return(
+  return (
     <>
-    <p>{counter}</p>
+      <div>
+        <div>{counter}</div>
+        <button onClick={() => setCounter(counter + 1)}>plus</button>
+        <button onClick={() => setCounter(0)}>zero</button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-
-export default AddCounter
+export default AddCounter;
