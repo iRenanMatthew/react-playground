6715b98c98f72eaab9496188ad63182431cc9b60
Renan Matthew Fajardo <renanmatthew206@gmail.com>
2025-09-20 01:16:34 +0800

    feat: "Refactored App2sjx with props and event handling"

--- src/Appv2.jsx
+++ src/Appv2.jsx
@@ -1,20 +1,55 @@
 import { useState } from "react";
 import "./App.css";
 
+const Button = (props) => {
+  return (
+    <>
+    <button onClick={props.onclick}>{props.name}</button>
+    </>
+  )
+}
+
+const DisplayButton = (props) =>{
+  return (
+    <>
+    <p>Counter: {props.counter}</p>
+    </>
+  )
+}
+ 
 const AddCounter = () => {
-  const [counter, setCounter] = useState(0);
-  setTimeout(() => {
+  const [counter, setCounter] = useState(1);
+  const [start, setStart] = useState(false);
+
+  const increment = () => {
     setCounter(counter + 1);
-  }, 1000);
+    setStart(true);
+  };
+
+  const decrement = () => {
+    if (counter - 1 < 0 && start === true) {
+      alert("Can't go beyond zero");
+      reset();
+      return;
+    }
+    setCounter(counter - 1);
+    setStart(true);
+  };
+
+  const reset = () => {
+    setCounter(0);
+    setStart(false);
+  };
 
   console.log("rendering", counter);
 
   return (
     <>
       <div>
-        <div>{counter}</div>
-        <button onClick={() => setCounter(counter + 1)}>plus</button>
-        <button onClick={() => setCounter(0)}>zero</button>
+        <DisplayButton counter={counter}/>
+        <Button onclick={increment} name="Plus +"></Button>
+        <Button onclick={decrement} name="Minus -"></Button>
+        <Button onclick={reset} name="Reset *"></Button>
       </div>
     </>
   );
